248b6e52cd98fe059382896241404a4f253799a7
Anna Kioko <annakioko00@gmail.com>
2024-03-03 16:25:16 +0300

    complete assignment

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,37 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        self.all.append(self)
+
+    def get_type(self):
+        return self._pet_type
+    
+    def set_type(self, pet_type):
+        if pet_type in self.PET_TYPES:
+            self._pet_type = pet_type
+        else:
+            raise Exception
+        
+    pet_type = property(get_type, set_type)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return Pet.all
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exception 
+           
+    def get_sorted_pets(self):
+        return sorted(Pet.all, key=lambda pet: pet.name)
